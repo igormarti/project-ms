00394b6f00489abee0300a91c52fe6f0698ebb6d
Igor Martins da Rocha <imartins@server01.foursys.local>
2022-06-07 14:09:16 -0300

    implemented payment roll mock

---
+++ hr-payroll/src/main/java/com/imr/hrpayroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.imr.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.imr.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	public Payment getPayment(Long workerId, Integer days) {
+		return new Payment("Bob", 200.00, days);
+	}
+}
